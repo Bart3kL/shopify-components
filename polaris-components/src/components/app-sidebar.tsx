e31a4e0b31b0f52b3fdb9200ed5647de7fced73e
Bartosz <blewandowski.2221@gmail.com>
2025-05-07 19:00:21 +0200

    chore: added Link instead of <a/> in nav

--- polaris-components/src/components/app-sidebar.tsx
+++ polaris-components/src/components/app-sidebar.tsx
@@ -14,6 +14,7 @@ import {
   SidebarMenuItem,
   SidebarRail,
 } from "@/src/components/ui/sidebar";
+import Link from "next/link";
 
 // This is sample data.
 const data = {
@@ -71,7 +72,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 {item.items.map((item) => (
                   <SidebarMenuItem key={item.title}>
                     <SidebarMenuButton asChild isActive={item.isActive}>
-                      <a href={item.url}>{item.title}</a>
+                      <Link href={item.url}>{item.title}</Link>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 ))}
